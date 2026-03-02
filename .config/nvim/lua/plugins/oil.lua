7288b1ef300b3a902b4d100cbfbc64b2c3dfa495
andreas.lebherz <andreas.lebherz@rcpe.at>
2026-03-02 09:08:34 +0100

    Added ctrl+p functionality to find file in project

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -2,32 +2,39 @@ return {
   "stevearc/oil.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("oil").setup({
+    local oil = require("oil")
+    oil.setup({
       default_file_explorer = true,
       delete_to_trash = true,
       skip_confirm_for_simple_edits = true,
       view_options = {
-        show_hidden = true, -- Crucial for seeing .config, .git, etc.
+        show_hidden = true,
         is_hidden_file = function(name, bufnr)
-          return vim.startswith(name, "..") -- Hide the '..' to keep it clean
+          return vim.startswith(name, "..")
         end,
       },
       keymaps = {
         ["<CR>"] = "actions.select",
         ["-"] = "actions.parent",
-        ["q"] = "actions.close", -- Press q to jump back to your code
-        ["<C-p>"] = "actions.preview", -- Preview the file under cursor in a popup
+        ["q"] = "actions.close",
+        
+        -- Disable C-p to overwrite as global Telescope works
+        ["<C-p>"] = false, 
+        -- Re-map preview to 'K' (standard Vim for 'hover/info')
+        ["K"] = "actions.preview",
+        -- Find files in current directory
         ["<leader>ff"] = function()
-          local current_dir = require("oil").get_current_dir()
+          local current_dir = oil.get_current_dir()
           require("telescope.builtin").find_files({
             cwd = current_dir,
             hidden = true,
             no_ignore = false,
           })
         end,
+        -- Live grep in current directory
         ["<leader>fg"] = function()
           require("telescope.builtin").live_grep({
-            cwd = require("oil").get_current_dir(),
+            cwd = oil.get_current_dir(),
           })
         end,
       },
